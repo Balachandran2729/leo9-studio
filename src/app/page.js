5d8f1aa9594346423be0fe1a09c2c86673c8eeda
Balachandran <balachandran2729@gmail.com>
2025-09-09 13:00:47 +0530

    basic bonoius point

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 "use client";
 import Navbar from './components/Navbar/page.jsx';
 import Hero from './components/Hero/page.jsx';
+import BlogCard from './components/BlogCard/page.jsx';
 import { useEffect, useState } from 'react';
 export default function Home() {
   const [isDarkMode, setIsDarkMode] = useState(true);
@@ -16,6 +17,7 @@ export default function Home() {
     <main>
       <Navbar isDarkMode={isDarkMode} setIsDarkMode={setIsDarkMode} />
       <Hero isDarkMode={isDarkMode} />
+      <BlogCard isDarkMode={isDarkMode} />
     </main>
   );
 }
